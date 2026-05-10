b3bf5dbe34031b603e1d91661278b5763c6e523d
L47-Coder <2360980084@qq.com>
2026-05-10 22:38:22 +0800

    新增LifeTime组件

--- Assets/Game/Component/Gameplay/Attack/ProjectileSpawn/ProjectileSpawnComponent.cs
+++ Assets/Game/Component/Gameplay/Attack/ProjectileSpawn/ProjectileSpawnComponent.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using Cysharp.Threading.Tasks;
 using UnityEngine;
 using VContainer;
 
@@ -7,30 +6,43 @@ public sealed partial class ProjectileSpawnComponentData
 {
     public string Key;
     public string ProjectilePrefabAddress = "Bullet";
+    public float SpawnsPerSecond = 5f;
 }
 
 public sealed partial class ProjectileSpawnComponent
 {
     private readonly ProjectileSpawnComponentData _componentData;
     [Inject] private readonly IPrefabManager _prefabManager;
+    [Inject] private readonly ITaskManager _taskManager;
+    private float _lastSpawnTime = float.NegativeInfinity;
 
-    public void Spawn(Vector2 startPosition, float angle)
+    public bool Spawn(Vector2 startPosition, float angle)
     {
-        if (_prefabManager == null) return;
-        SpawnAsync(startPosition, angle).Forget();
-    }
+        if (_prefabManager == null) return false;
 
-    private async UniTask SpawnAsync(Vector2 startPosition, float angle)
-    {
         string prefabKey = _componentData.ProjectilePrefabAddress;
-        if (string.IsNullOrWhiteSpace(prefabKey)) return;
+        if (string.IsNullOrWhiteSpace(prefabKey)) return false;
 
-        IPrefabHandle handle = await _prefabManager.LoadPrefabAsync(prefabKey);
-        Transform t = handle.GameObject.transform;
-        Vector3 spawnPosition = new(startPosition.x, startPosition.y, t.position.z);
-        Quaternion spawnRotation = Quaternion.Euler(0f, 0f, angle);
-        t.SetPositionAndRotation(spawnPosition, spawnRotation);
-        _prefabManager.SafeCallComponent<LinearMoveAlongDirectionComponent>(handle, "default", move =>
-            move.SetMoveDirection((Vector2)t.up));
+        float rate = _componentData.SpawnsPerSecond;
+        if (rate > 0f)
+        {
+            float minInterval = 1f / rate;
+            if (Time.time - _lastSpawnTime < minInterval)
+                return false;
+        }
+
+        _lastSpawnTime = Time.time;
+
+        _taskManager.CreateTask().ActionAsync(async (token) =>
+        {
+            IPrefabHandle handle = await _prefabManager.LoadPrefabAsync(_componentData.ProjectilePrefabAddress);
+            Transform t = handle.GameObject.transform;
+            Vector3 spawnPosition = new(startPosition.x, startPosition.y, t.position.z);
+            Quaternion spawnRotation = Quaternion.Euler(0f, 0f, angle);
+            t.SetPositionAndRotation(spawnPosition, spawnRotation);
+            _prefabManager.SafeCallComponent<LinearMoveAlongDirectionComponent>(handle, "default", move => move.SetMoveDirection((Vector2)t.up));
+        }).Run();
+
+        return true;
     }
 }
---
+++ Assets/Game/Component/Gameplay/Life/Lifetime/LifetimeComponent.cs
@@ -0,0 +1,29 @@
+﻿using VContainer;
+
+public sealed partial class LifetimeComponentData
+{
+    public string Key;
+    public int LifeTime = 5000;
+}
+
+public sealed partial class LifetimeComponent
+{
+    private readonly LifetimeComponentData _componentData;
+    [Inject] private readonly IPrefabManager _prefabManager;
+    [Inject] private readonly ITaskManager _taskManager;
+    private ITaskHandle _taskHandle;
+
+    protected override void OnAdd()
+    {
+        _taskHandle = _taskManager.CreateTask()
+            .WaitTime(_componentData.LifeTime)
+            .ActionAsync(async (token) =>
+            {
+                if(_prefabManager.TryGetHandle(GameObject, out var handle))
+                    await _prefabManager.ReleasePrefabAsync(handle);
+            })
+            .Run();
+    }
+
+    protected override void OnRemove() => _taskManager.StopTask(_taskHandle);
+}
